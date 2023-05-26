c7d8ca2fe7f562fffa261604e61dc411a288c272
Georges Bueno - Ogtic <georges.bueno@ogtic.gob.do>
2023-05-26 16:42:17 -0400

    productos principal

--- app/components/home/principalProducts.js
+++ app/components/home/principalProducts.js
@@ -8,15 +8,51 @@ export const PrincipalProducts = () => {
     return (
         <div className='w-full h-screen flex flex-row '>
             {/** 3 cuadros , uno grande 2 mediano */}
-            <div className='bg-blue-600 h-full w-2/3 flex items-center justify-center'>
-                <Image src={modelo} alt="modelo" />
+            <div className='bg-blue-600 h-full w-2/3 flex '>
+                <div className='absolute'>
+
+                    <h1 className='p-2 bg-white text-2xl font-bold' >Tiger T-Shirt</h1>
+
+                    <h3 className='p-2 bg-white text-2xl w-fit font-bold'>$25.00</h3>
+                </div>
+                <div className='w-full flex'>
+                    <div className='flex items-center justify-center w-full h-full '>
+                        <Image src={modelo} alt="modelo" className='w-fit h-3/4 ' />
+                    </div>
+                </div>
             </div>
             <div className='flex flex-col h-full  w-1/3'>
-                <div className='bg-red-600 h-1/2 w-full flex items-center justify-center '>
-                    <Image src={polo} alt="modelo" className='w-fit h-3/4 ' />     
+
+                <div className='h-1/2 bg-red-600 w-full flex'>
+                    <div className='absolute'>
+
+                        <h1 className='p-2 bg-white font-bold' >Tiger T-Shirt</h1>
+
+                        <h3 className='p-2 bg-white w-fit font-bold'>$20.00</h3>
+                    </div>
+                    <div className='bg-red-600    w-full flex '>
+
+                        <div className='flex items-center justify-center w-full h-full '>
+
+                            <Image src={polo} alt="modelo" className='w-fit h-3/4 ' />
+                        </div>
+                    </div>
                 </div>
-                <div className='bg-yellow-600 h-1/2 w-full flex items-center justify-center '>
-                    <Image src={vestido} alt="modelo"className='w-fit  h-3/4 ' />
+
+                <div className='h-1/2 bg-red-600 w-full flex'>
+                    <div className='absolute'>
+
+                        <h1 className='p-2 bg-white font-bold' >Tiger T-Shirt</h1>
+
+                        <h3 className='p-2 bg-white w-fit font-bold'>$25.00</h3>
+                    </div>
+                    <div className='bg-yellow-600 w-full flex'>
+
+                        <div className='flex items-center justify-center w-full h-full '>
+
+                            <Image src={vestido} alt="modelo" className='w-fit h-3/4 ' />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
